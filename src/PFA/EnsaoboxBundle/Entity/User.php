a666db52cf6d5beb00401e0826b091741bcda918
achraf <nasri.ach@gmail.com>
2015-04-26 20:17:56 +0100

    fosUserBundle installé

---
+++ src/PFA/EnsaoboxBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Achraf
+ * Date: 26/04/2015
+ * Time: 19:56
+ */
+
+namespace PFA\EnsaoboxBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
