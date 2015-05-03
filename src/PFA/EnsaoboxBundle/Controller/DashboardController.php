b5f14935b3b57a080694811bbf4c5562bc0c0444
achraf <nasri.ach@gmail.com>
2015-05-03 13:08:43 +0100

    site est maintenant privé,roles créés, droit de partage de document possible que par un professeur, espace d'admin accessible que par un administrateur

--- src/PFA/EnsaoboxBundle/Controller/DashboardController.php
+++ src/PFA/EnsaoboxBundle/Controller/DashboardController.php
@@ -3,11 +3,22 @@
 namespace PFA\EnsaoboxBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DashboardController extends Controller
 {
     public function indexAction()
     {
         return $this->render('PFAEnsaoboxBundle:dashboard:index.html.twig');
+    }
+
+    public function addDocumentAction()
+    {
+        return new response('ICI VOUS DEVELOPPEZ l\'ajout d\'un document, cette action n\'est accessible que par les professeurs');
+    }
+
+    public function adminAction()
+    {
+        return new Response('ICI VOUS DEVELOPPEZ la page l\'administrateur');
     }
 }
