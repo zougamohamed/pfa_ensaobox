2101adcf106537a4801e426abf932f12ddaa7853
achraf <nasri.ach@gmail.com>
2015-05-27 00:37:53 +0100

    finalisation

--- src/PFA/EnsaoboxBundle/Form/DocumentType.php
+++ src/PFA/EnsaoboxBundle/Form/DocumentType.php
@@ -10,10 +10,10 @@ class DocumentType extends AbstractType
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
-        ->add('filieres','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Filieres','property'=>'nomFiliere','multiple'  => false,'attr' => array('style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
-        ->add('classes','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Classes','property'=>'nomClasse','attr' => array('style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
-        ->add('matieres','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Matieres','property'=>'nomMatiere','attr' => array('style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
-        ->add('name','text',array('label'=>'Nouvelle matiére','required'    => false,'attr' => array('style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
+        ->add('filieres','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Filieres','property'=>'nomFiliere','multiple'  => false,'attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
+        ->add('classes','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Classes','property'=>'nomClasse','attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
+        ->add('matieres','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Matieres','property'=>'nomMatiere','attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
+        ->add('name','text',array('label'=>'Nouvelle matiére','required'    => false,'attr' => array('class' => 'form-control' ,'style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
         ->add('file','file',array('required' => true,'attr'     =>   array('style' => 'font-size:17px;')))
         ->add('envoyer','submit',array('attr' => array('class' => 'btn btn-shadow btn-success','style' => 'font-size:17px ')))
     ;
