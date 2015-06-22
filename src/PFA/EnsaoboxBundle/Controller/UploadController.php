5543aca285528f51c959755f54ac644364562e33
makranelhoucine <makranelhoucine@gmail.com>
2015-06-22 00:29:28 +0000

    dimanche push

--- src/PFA/EnsaoboxBundle/Controller/UploadController.php
+++ src/PFA/EnsaoboxBundle/Controller/UploadController.php
@@ -1,13 +1,11 @@
 <?php
-
 namespace PFA\EnsaoboxBundle\Controller;
-
 use PFA\EnsaoboxBundle\Entity\Matieres;
 use PFA\EnsaoboxBundle\Form\DocumentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use PFA\EnsaoboxBundle\Entity\Document;
 use Symfony\Component\HttpFoundation\Request;
-
+use Doctrine\ORM\EntityRepository;
 class UploadController extends Controller
 {
     public function ajouterAction(Request $request)
@@ -15,14 +13,15 @@ class UploadController extends Controller
         $userType= $this->getUser()->getRoles()[0];
         $request->getSession()->set('utilisateur',$userType);
         $matieresDeChaqueProf=array();
-
         $matiereEnregistreByNamex = $this->getDoctrine()->getRepository("PFAEnsaoboxBundle:Matieres")->findBy(array('professeur' => $this->getUser()->getUsername()));
         foreach($matiereEnregistreByNamex as $x)
         {
             $matieresDeChaqueProf[]= $x->getNomMatiere();
         }
         $matieresDeChaqueProf[]='NOUVELLE MATIERE';
+        $stop=false;
         $error="";
+        $info="";
         $userNameSession=$this->getUser()->getUsername();
         $newMatiere=new Matieres();
         $documentTeleccharger=new Document();
@@ -30,19 +29,26 @@ class UploadController extends Controller
         $form = $this->createFormBuilder($documentTeleccharger)
             ->add('filieres','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Filieres','property'=>'nomFiliere','multiple'  => false,'attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
             ->add('classes','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Classes','property'=>'nomClasse','attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
-            ->add('matieres','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Matieres','property'=>'nomMatiere','attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
+//            ->add('matieres','entity',array('class'=>'PFA\EnsaoboxBundle\Entity\Matieres','property'=>'nomMatiere','attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
 //            ->add('matieres', 'choice', array( 'choices' => $matieresDeChaqueProf, 'attr' => array('class' => 'form-control', 'style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ') ))
+            ->add('matieres','entity', array(
+                'class' => 'PFA\EnsaoboxBundle\Entity\Matieres','property'=>'nomMatiere','attr' => array('class' => 'form-control','style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  '),
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->where('u.professeur=:prof')
+                        ->setParameter('prof',$this->getUser()->getUsername())
+                        ->orderBy('u.nomMatiere', 'DESC');
+                },
+            ))
             ->add('name','text',array('label'=>'Nouvelle matiére','required'    => false,'attr' => array('class' => 'form-control' ,'style' => 'background-color:rgb(250, 255, 189);color:black;font-size:15px;  ')))
             ->add('file','file',array('required' => true,'attr'     =>   array('style' => 'font-size:17px;')))
             ->add('envoyer','submit',array('attr' => array('class' => 'btn btn-shadow btn-success','style' => 'font-size:17px ')))
             ->getForm();
         //----------------------------------------------------------------------
-
         if ($request->isMethod('POST'))
         {
             $form->handleRequest($request);
             if ($form->isValid()) {
-
                 if($form['file']->getData()!=null)
                 {
                     if($form['name']->getData()!=null)
@@ -59,7 +65,9 @@ class UploadController extends Controller
                         else
                         {
                             global $error;
-                            $error='le matiere existe deja . choisissez votre a partir de la liste ';
+                            $error='la matière existe déja . choisissez la matière à partir de la liste ';
+                            global $stop;
+                            $stop=true;
 
                         }
                     }
@@ -84,9 +92,8 @@ class UploadController extends Controller
                         }
                         else
                         {
-
-                            global $error;
-                            $error = "le fichier est bien telecharger";
+                            global $info;
+                            $info = "le fichier est bien téléchargé";
                             if($form['name']->getData()==null  && $form['matieres']->getData()!=null)
                             {
                                 $documentTeleccharger->name='empty';
@@ -97,14 +104,12 @@ class UploadController extends Controller
                                 $em = $this->getDoctrine()->getManager();
                                 $em->persist($documentTeleccharger);
                                 $em->flush();
-
                             }
                             elseif($form['name']->getData()!=null &&($form['matieres']->getData()!=null || $form['matieres']->getData()==null ))
                             {
                                 $documentTeleccharger->upload($form['filieres']->getData()->getNomFiliere() . '/' . $form['classes']->getData()->getNomClasse(). '/' .$this->getUser()->getUsername() . '/' . $form['name']->getData());
                                 global $error;
                                 $documentTeleccharger->setProfesseur($userNameSession);
-
                                 $em = $this->getDoctrine()->getManager();
                                 $em->persist($documentTeleccharger);
                                 $em->flush();
@@ -112,20 +117,16 @@ class UploadController extends Controller
                             else
                             {
                                 global $error;
-                                $error='vous devez choisir votre matiere si il nexiste dans la liste vous devez linserer ';
+                                $error='vous devez choisir votre matière si il n\'existe dans la liste vous devez l\'insérer ';
                             }
-
-
-
                             $this->redirect($this->generateUrl('pfa_ensaobox_ajouter_files'));
-//                      return $this->redirect($this->generateUrl('pfa_ensaobox_ajouter_files', array("doc"=>$documentEnregistre)));
-//                      return new response('hhhhh'.print_r($documentTeleccharger));
+
                         }
                     }
                     else
                     {
-                        global $error;
-                        $error = "le fichier est bien telecharger";
+                        global $info;
+                        $info = "le fichier est bien téléchargé";
                         if($form['name']->getData()==null  && $form['matieres']->getData()!=null)
                         {
                             $documentTeleccharger->name='empty';
@@ -136,14 +137,12 @@ class UploadController extends Controller
                             $em = $this->getDoctrine()->getManager();
                             $em->persist($documentTeleccharger);
                             $em->flush();
-
                         }
                         elseif($form['name']->getData()!=null &&($form['matieres']->getData()!=null || $form['matieres']->getData()==null ))
                         {
                             $documentTeleccharger->upload($form['filieres']->getData()->getNomFiliere() . '/' . $form['classes']->getData()->getNomClasse(). '/' .$this->getUser()->getUsername() . '/' . $form['name']->getData());
                             global $error;
                             $documentTeleccharger->setProfesseur($userNameSession);
-
                             $em = $this->getDoctrine()->getManager();
                             $em->persist($documentTeleccharger);
                             $em->flush();
@@ -151,31 +150,26 @@ class UploadController extends Controller
                         else
                         {
                             global $error;
-                            $error='vous devez choisir votre matiere si il nexiste dans la liste vous devez linserer ';
+                            $error='vous devez choisir votre matière si il n\'éxiste dans la liste vous devez l\'insérer ';
                         }
-
-
-
                         $this->redirect($this->generateUrl('pfa_ensaobox_ajouter_files'));
-//                      return $this->redirect($this->generateUrl('pfa_ensaobox_ajouter_files', array("doc"=>$documentEnregistre)));
-//                      return new response('hhhhh'.print_r($documentTeleccharger));
 
                     }
-
                 }
                 else
                 {
                     global $error;
-                    $error="veulliez telecharger un fichier s'il vous plait";
+                    $error="veulliez télécharger un fichier s'il vous plait";
                 }
             }
         }
-//        $documentEnregistre2=$this->getDoctrine()->getRepository("PFAEnsaoboxBundle:Document")->findAll();
         $documentEnregistre = $this->getDoctrine()->getRepository("PFAEnsaoboxBundle:Document")->findBy(array('professeur' => $userNameSession));
-
         return $this->render('PFAEnsaoboxBundle:addFiles:addFiles.html.twig', array(
-            'form' => $form->createView(),"doc"=>$documentEnregistre,'error'=>$error,'userNameSession'=>$userNameSession
+            'form' => $form->createView(),
+            "doc"=>$documentEnregistre,
+            'error'=>$error,
+            'info'=>$info,
+            'userNameSession'=>$userNameSession
         ));
-
     }
 }
